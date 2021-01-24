bd4fcec663613de224a7044e0f2a80d922cd7b01
Sajanader <sajanader93@gmail.com>
2021-01-24 11:18:50 +0200

    styling

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,17 @@
-  
 import Link from 'next/link'
+import styles from '../styles.module.css'
 
 export default function Header(){
     return(
         <header>
             <nav>
                 <ul>
-                        <Link href='/about'><a>About us</a></Link>
+                        <Link href='/about'>
+                            <a className = {styles.link} >About us</a>
+                            </Link>
 
-                        <Link href='/'><a>Home</a></Link>
+                        <Link href='/'>
+                            <a className = {styles.link}>Home</a></Link>
 
                 </ul>
             </nav>
